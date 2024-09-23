29564ef8abd7d88d777d1e79e94e6a86b7fef9d4
rox-crxd <roxan984231211@gmail.com>
2024-09-22 20:07:40 -0500

    prueba

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.CodeDom;
+using System.Data.SqlClient;
+
+namespace Prueba1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena = ConfigurationManager.ConnectionStrings["cnx"].ConnectionString;
+
+            Console.WriteLine(cadena);
+            SqlConnection conn = null;
+            SqlCommand cmd;
+            SqlDataReader reader = null;
+            try
+            {
+                conn = new SqlConnection(cadena);
+                conn.Open();
+                cmd = new SqlCommand("usp_prueba", conn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.Clear();
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader.GetString(1));
+                }
+                 }
+                catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                //Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                reader.Close();
+                conn.Close();
+            }
+            Console.ReadKey();
+            }
+    }
+}
